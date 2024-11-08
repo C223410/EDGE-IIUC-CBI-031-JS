29c90588dcda520744aa90f7dc87a4f68e55f858
Ahz Mahi <mahzmahi4719@gmail.com>
2024-11-08 11:36:57 +0600

    started JS

---
+++ JS-01/alert.js
@@ -0,0 +1,9 @@
+// alert('Hello, world! from external file');
+// alert('hello')
+// alert(3 +
+//     1
+//     + 2)
+
+alert("Hello")
+
+[1, 2].forEach(alert);
---
+++ JS-01/type.js
@@ -0,0 +1,7 @@
+// let name = "Ilya";
+let name;
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
